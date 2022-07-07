88bf7b47179fc88834efc99b157747d1eea84a1b
Lindsay Bruley <muzikismylife13@gmail.com>
2022-07-06 21:38:28 -0700

    Move buttons, style overall calculator, add start of Javascript

---
+++ script.js
@@ -0,0 +1,39 @@
+function digitSelector() {
+    //create listener for digits being pushed and add number to the appropriate variable
+}
+
+function createA () {
+
+}
+
+function createB () {
+
+}
+
+function addition(a, b) {
+    return a + b;
+    //listen for + button click
+}
+
+function subtraction (a, b) {
+    return a - b;
+    // listen for - button click
+}
+
+function multiply (a, b) {
+    result a * b;
+    // listen for * button click
+}
+
+function divide (a, b) {
+    result a / b;
+    // listen for / button click
+}
+
+function clearLastClick() {
+    
+}
+
+function clearCalc() {
+
+}
